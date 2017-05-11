b4376a29467ab1b9edf4be28f9b151db180897f7
yayrat <yayrat@gmail.com>
2017-05-11 09:39:47 +0300

    '0.0.5'

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pkill -f geth;
-nohup geth --datadir "./" --mine --rpc --rpcport 8545 --rpcaddr "0.0.0.0" rpccorsdomain "*" --networkid "10101010" > out.txt &
+nohup geth --datadir "./" --mine --rpc --nodiscover --rpcport 8545 --rpcaddr "0.0.0.0" rpccorsdomain "*" --networkid "10101010" > out.txt &
 
